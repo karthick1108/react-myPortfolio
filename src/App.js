1d573d9c877aa546a23c4bc981335d9de39fd154
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-08-31 21:05:18 +1000

    created components and added navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -8,22 +10,23 @@ function App() {
       <Layout>
         <Header title="Title" scroll>
           <Navigation>
-            <a href="/">Rajasekaran</a>
-            <a href="/">Praveen</a>
-            <a href="/">Sriram</a>
-            <a href="/">Karthick</a>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
         <Drawer title="Title">
           <Navigation>
-            <a href="/">Muthu</a>
-            <a href="/">Jaanvi</a>
-            <a href="/">Uma</a>
-            <a href="/">Vidhya</a>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
           </Navigation>
         </Drawer>
         <Content>
           <div className="page-content" />
+          <Main />
         </Content>
       </Layout>
     </div>
