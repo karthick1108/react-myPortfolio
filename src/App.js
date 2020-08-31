424218f11fd1363336b7d366f986cb2e8707cf1e
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-08-31 19:02:32 +1000

    added react-mdl and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi this is Karthick</h1>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <a href="/">Rajasekaran</a>
+            <a href="/">Praveen</a>
+            <a href="/">Sriram</a>
+            <a href="/">Karthick</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Muthu</a>
+            <a href="/">Jaanvi</a>
+            <a href="/">Uma</a>
+            <a href="/">Vidhya</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+        </Content>
+      </Layout>
     </div>
   );
 }
